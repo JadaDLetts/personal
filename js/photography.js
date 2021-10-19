9d68320702236c4e1d84728e20029e0a1573728b
JadaDLetts <45326648+JadaDLetts@users.noreply.github.com>
2021-10-19 16:16:59 -0400

    updated html and js of photography

--- js/photography.js
+++ js/photography.js
@@ -1,17 +1,20 @@
 //using sIndex to represent the current slide being shown
-let sIndex = 0;
+let index = 0;
+console.log("index: " + index);
+//getting the array of slides
+//retrieves all elements that go by the class name slides
+let slides = document.getElementsByClassName("slides");
+//getting the array of dots
+//retrieves all elements that go by the class name dot
+let dots = document.getElementsByClassName("dot");
+
+
 //starts slideshow of photos
+console.log("starting slideshow");
+
 showSlides();
 
-//used to show all of the photos in the slideshow on a timer
-function showSlides() {
-    //getting the array of slides
-    //retrieves all elements that go by the class name slides
-    let slides = document.getElementsByClassName("slides");
-    //getting the array of dots
-    //retrieves all elements that go by the class name dot
-    let dots = document.getElementsByClassName("dot");
-
+function emptyDisplay() {
     //for loop to make sure only one photo can be displayed at one time
     //will not have to worry about finding which picture is currently being displayed
     for (let i = 0; i < slides.length; i++) {
@@ -23,77 +26,60 @@ function showSlides() {
     for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+}
 
-    //checking that the current sIndex is within range of the number of photos
-    if (sIndex >= slides.length) {
-        sIndex = 0
-    } else if (sIndex < 0) {
-        sIndex = slides.length - 1
-    }
-
+function addToDisplay() {
     //displays the slide selected
-    slides[sIndex].style.display = "block";
+    slides[index].style.display = "block";
     //displays the dot selected/ the dot associated with the slide currently displayed
-    dots[sIndex].className += " active";
+    dots[index].className += " active";
+}
+
+function checkSIndex() {
+    //checking that the current sIndex is within range of the number of photos
+    if (index >= slides.length) {
+        index = 0
+    } else if (index < 0) {
+        index = slides.length - 1
+    }
+}
+
+//used to show all of the photos in the slideshow on a timer
+function showSlides() {
+    showSlide(index);
 
     //changing the image every 7 seconds
     setTimeout(showSlides, 7000);
     //increasing the sIndex so that the slideshow does not stay on one image
     //setTimeout will not increment the sIndex
-    sIndex++;
+    index++;
 }
 
 //changes the current slide based on the dot pressed
 //param int is given by the dot pressed
 //calls show slide to ensure that the photo associated with the dot is shown
 function currentSlide(int) {
-    sIndex = int;
-    showSlide(sIndex);
+    index = int;
+    showSlide(index);
 }
 
+//
 //used to show individual slides
 //parameter int represents the number of the slide to be shown
 function showSlide(int) {
-
     //initiating sIndex equal to the given parameter
-    sIndex = int;
+    index = int;
 
-    //getting the array of slides
-    //retrieves all elements that go by the class name slides
-    let slides = document.getElementsByClassName("slides");
-    //getting the array of dots
-    //retrieves all elements that go by the class name dot
-    let dots = document.getElementsByClassName("dot");
+    checkSIndex();
 
+    emptyDisplay();
 
-    //checking that the current sIndex is within range of the number of photos
-    if (sIndex >= slides.length) {
-        sIndex = 0
-    } else if (sIndex < 0) {
-        sIndex = slides.length - 1
-    }
-
-    //for loop to make sure only one photo can be displayed at one time
-    //will not have to worry about finding which picture is currently being displayed
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    //for loop to make sure only one dot can be highlighted at one time
-    //will not have to worry about finding which dot is currently being highlighted
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    //displays the slide selected
-    slides[sIndex].style.display = "block";
-    //displays the dot selected/ the dot associated with the slide currently displayed
-    dots[sIndex].className += " active";
+    addToDisplay();
 }
 
 //ensures that when next/prev button is clicked the next slide is properly shown
 //param int is given by the buttons
 //calls show slide to show the next slide based on param int
 function nextSlide(int) {
-    showSlide(sIndex += int);
+    showSlide(index += int);
 }
