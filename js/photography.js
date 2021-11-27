6e6d7e09cee44610a109d854a4ee22b7f557c4e5
Jada Letts <45326648+JadaDLetts@users.noreply.github.com>
2021-11-27 11:49:41 -0500

    deleted common functions

--- js/photography.js
+++ js/photography.js
@@ -12,76 +12,16 @@ let dots = document.getElementsByClassName("dot");
 //starts slideshow of photos
 showSlides();
 
-//function to help display the current slide
-function emptyDisplay() {
-	//for loop to make sure only one photo can be displayed at one time
-	//will not have to worry about finding which picture is currently being displayed
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	
-	//for loop to make sure only one dot can be highlighted at one time
-	//will not have to worry about finding which dot is currently being highlighted
-	for (let i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-}
-
-//function to help  display the current slide
-function addToDisplay() {
-	//displays the slide selected
-	slides[index].style.display = "block";
-	//displays the dot selected/ the dot associated with the slide currently displayed
-	dots[index].className += " active";
-}
-
-//function to check what slide the program is currently on
-function checkSIndex() {
-	//checking that the current sIndex is within range of the number of photos
-	if (index >= slides.length) {
-		index = 0
-	} else if (index < 0) {
-		index = slides.length - 1
-	}
-}
 
 //used to show all of the photos in the slideshow on a timer
 function showSlides() {
 	showSlide(index);
-	
+
+	index++;
+
 	//changing the image every 7 seconds
 	setTimeout(showSlides, 7000);
 	//increasing the sIndex so that the slideshow does not stay on one image
 	//setTimeout will not increment the sIndex
-	index++;
-}
 
-//changes the current slide based on the dot pressed
-//param int is given by the dot pressed
-//calls show slide to ensure that the photo associated with the dot is shown
-function currentSlide(int) {
-	index = int;
-	showSlide(index);
-}
-
-//
-//used to show individual slides
-//parameter int represents the number of the slide to be shown
-function showSlide(int) {
-	//initiating sIndex equal to the given parameter
-	index = int;
-	
-	//checking the index of the slide
-	checkSIndex();
-	
-	//calling these two functions to display the proper slide
-	emptyDisplay();
-	addToDisplay();
-}
-
-//ensures that when next/prev button is clicked the next slide is properly shown
-//param int is given by the buttons
-//calls show slide to show the next slide based on param int
-function nextSlide(int) {
-	showSlide(index += int);
 }
