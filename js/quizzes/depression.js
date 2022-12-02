4ef0aa9552a5e8fef2b120a7f3fce59676096a87
Jada <jadaletts@gmail.com>
2022-12-02 00:19:53 -0500

    reverted index changes, added depression questions

---
+++ js/quizzes/depression.js
@@ -0,0 +1,101 @@
+const questions = [
+    {
+        question: 'Trouble falling or staying asleep, or sleeping too much',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'Feeling tired/having little to no energy',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'Overeating / undereating',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'Little to no interest/pleasure in doing things that you usually like doing',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'Feeling hopeless, down or miserable',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'Thoughts of hurting yourself or others / thoughts that you would be better off' +
+            ' dead, or' +
+            ' of hurting' +
+            ' yourself',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'Trouble concentrating on things, such as reading or watching television',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'Feeling bad about yourself, feeling that you are a failure or' +
+            ' feeling that you have let yourself or your family down',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'Moving or speaking slowly enough that you or others could have' +
+            ' noticed / the opposite - being fidgety or restless enough that you or others' +
+            ' could have noticed that you have been moving' +
+            ' around a lot more than usual',
+        answers: {
+            1: 'Not at all',
+            2: 'Several days',
+            3: 'More than half the days',
+            4: 'Nearly Every day'
+        }
+    },
+    {
+        question: 'If you checked off anything other than not at all, how difficult' +
+            ' have these problems' +
+            ' made it for you at work, home, or with other people?',
+        answers: {
+            1: 'Not difficult at all',
+            2: 'Somewhat difficult',
+            3: 'Very difficult',
+            4: 'Extremely difficult'
+        }
+    },
+]
